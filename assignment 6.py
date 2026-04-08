3f6a818bc6dff9f5cbee666f06b877ae653bd6bd
Pawar Vaishnavi <pawarvaishnavib2002@gmail.com>
2026-04-08 14:03:04 +0530

    add assignment 6 succesfully

---
+++ assignment 6.py
@@ -0,0 +1,15 @@
+# 6. Find list of common unique items from two list. and show in increasing order
+
+# 	Input
+
+# 	num1 = [23,45,67,78,89,34]
+# 	num2 = [34,89,55,56,39,67]
+# 	Output:
+
+# 	[34, 67, 89]
+num1 = [23, 45, 67, 78, 89, 34]
+num2 = [34, 89, 55, 56, 39, 67]
+
+result = sorted(set(num1) & set(num2))
+
+print(result)
